7b6b4410eae7833e41a3b194623ca833b017f27d
Mez0ry <mez0ry@mail.ru>
2025-01-11 15:21:54 +0100

    Refactoring

--- Chess/Board.cpp
+++ Chess/Board.cpp
@@ -2,13 +2,20 @@
 #include "Lerp.hpp"
 #include "Easing.hpp"
 
-Board::Board(ObjectSize board_size)
+#include <cctype>
+#include <cstdlib>
+
+#include "AABB.hpp"
+#include <unordered_map>
+#include "EventHandler.hpp"
+
+Board::Board(ObjectSize board_size) : m_TeamToMove(Team::WHITE)
 {
     m_BoardSize = board_size;
 }
 
 
-Board::Board(const Core::Ref<Renderer> renderer,const Core::Ref<Window> window,const std::string& path,ObjectSize one_square_src_size, ObjectSize board_size) : m_Renderer(renderer), m_Window(window)
+Board::Board(const Core::Ref<Renderer> renderer,const Core::Ref<Window> window,const std::string& path,ObjectSize one_square_src_size, ObjectSize board_size) : m_TeamToMove(Team::WHITE), m_Renderer(renderer), m_Window(window), m_BoardSize(board_size)
 {
     Setup(renderer,window,path,one_square_src_size);
 }
@@ -23,6 +30,7 @@ void Board::Setup(const Core::Ref<Renderer> renderer,const Core::Ref<Window> win
     auto[win_w,win_h] = window->GetWindowSize();
 
     m_OneSquareSizeSrc = one_square_src_size;
+    m_EntityAdjustedSize = ObjectSize(m_OneSquareSizeSrc.GetWidth() * 0.8f, m_OneSquareSizeSrc.GetHeight() * 0.8f);
     
     m_WhiteSquareTexture.LoadTexture(renderer,path);
 
@@ -39,15 +47,17 @@ void Board::Setup(const Core::Ref<Renderer> renderer,const Core::Ref<Window> win
         m_BoardTopLeft.y = dy;
     });
 
+    m_PieceOpacityKF.Setup(0.25f,[&](float t){
+        uint8_t alpha = Stellar::Lerp(0, 256,Stellar::Easing::EaseInSine(t));
+        for(auto& piece : m_Pieces){
+            SDL_SetTextureAlphaMod(piece->GetTexture(),alpha);
+        }
+    });
+
     OnResize(window);
 
     m_BoardTopLeft.x = m_BoardTopLeftMemento.GetState().x;
     m_BoardTopLeft.y = -m_FullBoardSize.GetHeight();
-}
-
-void Board::LoadSquareTexture(const std::string& path,ObjectSize square_src_size)
-{
-    
 }
 
 void Board::OnCreate()
@@ -69,9 +79,25 @@ void Board::OnResize([[maybe_unused]] const Core::Ref<Window> window)
     tmp.y = ((win_h -  max_size) / 2);
 
     m_BoardTopLeftMemento.Save(tmp);
+    m_BoardTopLeft.x = tmp.x;
 
+    if(m_BoardTopLeftKF.IsFinished()){
+        m_BoardTopLeft = tmp;
+    }
+    
     m_BlackSquareTexture.SetSize(m_OneSquareSize);
     m_WhiteSquareTexture.SetSize(m_OneSquareSize);
+
+    m_EntityAdjustedSize = ObjectSize(m_OneSquareSize.GetWidth() * 0.8f,m_OneSquareSize.GetHeight() * 0.8f);
+
+    for(auto& piece : m_Pieces){
+        if(!piece && piece->IsKilled()) 
+            continue;
+
+        piece->SetSize(m_EntityAdjustedSize);
+        SetTextureEntityPosition(piece);
+    }
+
 }
 
 void Board::OnDestroy()
@@ -81,12 +107,70 @@ void Board::OnDestroy()
 
 void Board::HandleInput([[maybe_unused]] const Core::Ref<EventHandler> event_handler)
 {
+    const auto& cursor_pos = MouseInput::GetMousePosition();
+
+    //@TODO  lerp from start_drag_pos to final_entity_pos
+   static Vec2i initial_drag_pos, target_entity_pos;
+
+    if(MouseInput::GetMouseState(MouseState::MOUSE_BUTTON_DOWN)){
+        if(MouseInput::IsPressed(SDL_BUTTON_LEFT)){
+            for(auto& piece : m_Pieces){
+                if(!piece || piece->IsKilled())
+                    continue;
+                
+                auto& piece_texture = piece->GetTexture();
+
+                bool is_on_object = AABB::PointIsOnObject(cursor_pos,piece_texture.GetRect());
+                if(is_on_object){
+                    initial_drag_pos = piece->GetTexture().GetPosition();
+                    piece->Drag();
+                }
+            }
+        }
+    }
     
+    if(MouseInput::GetMouseState(MouseState::MOUSE_BUTTON_UP)){
+        if(MouseInput::IsReleased(SDL_BUTTON_LEFT)){
+            Vec2i diff = cursor_pos - m_BoardTopLeft;
+
+            Vec2i move_to = {diff.x / m_OneSquareSize.GetWidth(),diff.y / m_OneSquareSize.GetHeight()};
+            
+            for(auto& piece : m_Pieces){
+                if(!piece || piece->IsKilled())
+                    continue;
+
+                if(piece->IsDragging()){
+
+                    if(IsMakeableMove(piece,move_to)){
+                        MakeMove(piece,move_to);
+
+                        Vec2i curr_relative_pos = GetRelativePos((*this),move_to);
+                        piece->ApplySmoothMove(initial_drag_pos,curr_relative_pos,move_to,1.f,Stellar::Easing::EaseInOutSine);
+                    }
+
+                    piece->StopDragging();
+                }
+            }
+        }
+
+
+    }
 }
 
 void Board::Update(float dt)
 {
     m_BoardTopLeftKF.Update(dt);
+
+    if(m_BoardTopLeftKF.GetElapsedFrames() > 2.0f){
+        m_PieceOpacityKF.Update(dt);
+    }
+    
+    for(auto& piece : m_Pieces){
+        if(!piece || piece->IsKilled())
+            continue;
+        
+        piece->Update(dt);
+    }
 }
 
 void Board::Render(const Core::Ref<Renderer> renderer)
@@ -111,4 +195,180 @@ void Board::Render(const Core::Ref<Renderer> renderer)
             
         }
     }
+
+    for(auto& piece : m_Pieces){
+        if(piece && !piece->IsKilled()){
+            if(!piece->IsSmoothMoving()){
+                SetTextureEntityPosition(piece);
+            }
+            piece->Render(renderer);
+        }
+    }
+
+}
+
+Vec2i Board::GetBoardTopLeft() const
+{
+    return m_BoardTopLeft;
+}
+
+ObjectSize Board::GetBoardSize() const
+{
+    return m_BoardSize;
+}
+
+ObjectSize Board::GetOneSquareSize() const
+{
+    return m_OneSquareSize;
+}
+
+bool Board::EntityIsOutOfBorder(const Core::Ref<IEntity> entity) const
+{
+   const auto& entity_pos = entity->GetPosition();
+   auto brd_tl_state = m_BoardTopLeftMemento.GetState();
+
+   Rect rect(brd_tl_state,m_FullBoardSize);
+   
+   return (AABB::PointIsOnObject(entity_pos,rect));
+}
+
+//"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+void Board::LoadBoardFromFen(const char* fen)
+{  
+    Vec2i new_pos;
+    new_pos.x = 0;
+    new_pos.y = m_BoardSize.GetHeight() - 1;
+
+    char row_symbol = '/';
+    int read_idx = 0;
+
+    std::unordered_map<Core::Ref<Piece>,char> handled_pieces;
+
+    auto is_piece_symbol = [&](char symbol) -> bool{
+        auto it = m_cPiecesMap.find(symbol);
+        return (it != m_cPiecesMap.end());
+    };
+    
+    size_t space_cnt = 0;
+    for(int i = 0; i < strlen(fen);++i){
+        if(fen[i] == ' '){
+            ++space_cnt;
+        }
+
+        auto requirement = [&](const Core::Ref<Piece> piece) -> bool{
+            auto piece_type = piece->GetPieceType();
+            auto team = piece->GetTeam();
+
+            char piece_type_symbol = (team == Team::WHITE) ? toupper(static_cast<char>(piece->GetPieceType()) ) : static_cast<char>(piece->GetPieceType()) ;
+            return (fen[i] == piece_type_symbol && handled_pieces.count(piece) == 0);
+        };
+
+
+        auto founded_piece = FindPiece(requirement);
+
+        if(std::isdigit(fen[i])){
+            int digit = std::atoi(&fen[i]);
+            new_pos.x += digit;
+        }else if(fen[i] == '/'){
+            new_pos.x = 0;
+            --new_pos.y;
+        }else if(founded_piece && space_cnt < 1){
+            founded_piece->SetPosition(new_pos);
+            ++new_pos.x;
+            
+            founded_piece->Revive();
+            handled_pieces.insert(std::make_pair(founded_piece,fen[i]));
+        }
+
+    }
+ 
+}
+
+std::string Board::GenerateBoardFen()
+{
+    std::string fen_res;
+
+    return fen_res;
+}
+
+void Board::SetTextureEntityPosition(const Core::Ref<IEntity> &entity)
+{
+    if(!entity || EntityIsOutOfBorder(entity)){
+        return;
+    }
+
+    auto& entity_texture = entity->GetTexture();
+    
+    Vec2i relative_pos = GetRelativePos((*this),entity);
+    
+    entity_texture.SetPosition(relative_pos);
+}
+
+void Board::AddPiece(const Core::Ref<Piece> piece) {
+    piece->SetSize(m_EntityAdjustedSize);
+    
+    m_Pieces.push_back(piece);
+}
+
+void Board::RemovePiece(const Core::Ref<Piece> piece) {
+    m_Pieces.erase(std::remove_if(m_Pieces.begin(),m_Pieces.end(),[&](const Core::Ref<Piece> img_piece){
+        return (img_piece == piece);
+    }),m_Pieces.end());
+}
+
+void Board::KillPieceAt(const Vec2i& square) {
+    auto piece = FindPiece([&](const Core::Ref<Piece> piece){
+        return (piece->GetPosition() == square);
+    });
+
+    if(piece && !piece->IsKilled()){
+        piece->Kill();
+    }
+    
+}
+
+Core::Ref<Piece> Board::FindPiece(const std::function<bool(const Core::Ref<Piece>)>& pred) const
+{
+    auto it = std::find_if(m_Pieces.begin(),m_Pieces.end(),pred);
+    return (it != m_Pieces.end() ? (*it) : nullptr);
+}
+
+Vec2i Board::GetRelativePos(Board &board, const Vec2i &board_pos)
+{
+    Vec2i board_tl = board.GetBoardTopLeft();
+    
+    ObjectSize one_square_size = board.GetOneSquareSize();
+    
+    Vec2i relative_pos;
+    
+    relative_pos.x = board_tl.x + board_pos.x * one_square_size.GetWidth();
+    relative_pos.y = board_tl.y + board_pos.y * one_square_size.GetHeight();
+    
+    ObjectSize offset(one_square_size.GetWidth() * 0.2f, one_square_size.GetHeight() * 0.2f);
+
+    relative_pos.x += (offset.GetWidth() / 2);
+    relative_pos.y += (offset.GetHeight() / 2);
+
+    return relative_pos;
+}
+
+Vec2i Board::GetRelativePos(Board &board, const Core::Ref<IEntity> entity)
+{
+    Vec2i ent_pos = entity->GetPosition();
+    ObjectSize one_square_size = board.GetOneSquareSize();
+
+    Vec2i relative_ent_pos = GetRelativePos(board,ent_pos);
+    
+    return relative_ent_pos;
+}
+
+bool Board::IsMakeableMove(const Core::Ref<Piece> piece, Vec2i move_to)
+{
+    return true;
+}
+
+void Board::MakeMove(const Core::Ref<Piece> piece, Vec2i move)
+{
+    //piece->SetPosition(move);
+
 }
--- Chess/Board.hpp
+++ Chess/Board.hpp
@@ -1,17 +1,24 @@
 #ifndef __BOARD_HPP__
 #define __BOARD_HPP__
-#include "Matrix.hpp"
+#include <vector>
+#include <functional>
+#include <unordered_map>
+
 #include "ObjectSize.hpp"
 
 #include "Window.hpp"
 #include "Renderer.hpp"
 
-#include "Square.hpp"
-
 #include "Memento.hpp"
 #include "KeyFrame.hpp"
 
+/**@brief pieces/entities */
+#include "Piece.hpp"
+
+
 class EventHandler;
+
+constexpr const char* g_starting_position_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
 class Board{
 private:
@@ -23,8 +30,6 @@ public:
     ~Board();
 
     void Setup(const Core::Ref<Renderer> renderer,const Core::Ref<Window> window,const std::string& path,ObjectSize one_square_src_size);
-    
-    void LoadSquareTexture(const std::string& path, ObjectSize square_src_size);
 
     void OnCreate();
     void OnResize([[maybe_unused]] const Core::Ref<Window> window);
@@ -33,13 +38,73 @@ public:
     void HandleInput([[maybe_unused]] const Core::Ref<EventHandler> event_handler);
     void Update(float dt);
     void Render(const Core::Ref<Renderer> renderer);
+public:
+    Vec2i GetBoardTopLeft() const;
+    /**
+     * @brief get the board size
+     * @returns amount of squares by width and height
+     */
+    ObjectSize GetBoardSize() const;
+
+    ObjectSize GetOneSquareSize() const;
+
+public:
+    
+    bool EntityIsOutOfBorder( const Core::Ref<IEntity> entity) const;
+
+    void LoadBoardFromFen(const char* fen);
+    std::string GenerateBoardFen();
+
+    void SetTextureEntityPosition(const Core::Ref<IEntity>& entity);
+public:
+/**
+ * @brief adds piece to the board
+*/
+    void AddPiece(const Core::Ref<Piece> piece);
+/**
+ * @brief removes piece from the board
+*/
+    void RemovePiece(const Core::Ref<Piece> piece);
+
+/**
+ * @brief kills the Piece
+*/
+    void KillPieceAt(const Vec2i& square);
+
+/**
+ * @brief searches for the piece
+ * @returns on success Piece otherwise nullptr
+ * @param pred predicate to be used in 'find' algo
+*/
+    Core::Ref<Piece> FindPiece(const std::function<bool(const Core::Ref<Piece>)>& pred) const;
+
+    static Vec2i GetRelativePos(Board &board, const Vec2i& board_pos);
+    
+    static Vec2i GetRelativePos(Board& board,const Core::Ref<IEntity> entity);
+
+public:
+    bool IsMakeableMove(const Core::Ref<Piece> piece,Vec2i move_to);
+    void MakeMove(const Core::Ref<Piece> piece, Vec2i move);
+
+private:
+    std::vector<Core::Ref<Piece>> m_Pieces;
+    Team m_TeamToMove;
+    const std::unordered_map<char,PieceType> m_cPiecesMap = {{'p',PieceType::PAWN},
+                                                             {'r', PieceType::ROOK},
+                                                             {'n', PieceType::KNIGHT},
+                                                             {'b',PieceType::BISHOP},
+                                                             {'q',PieceType::QUEEN},
+                                                             {'k',PieceType::KING}};
 private:
     Texture m_WhiteSquareTexture, m_BlackSquareTexture;
-    ObjectSize m_BoardSize; // matrix size
+    ObjectSize m_BoardSize; // squares size
     ObjectSize m_OneSquareSize,m_OneSquareSizeSrc;
+    ObjectSize m_EntityAdjustedSize;
     ObjectSize m_FullBoardSize;
     Vec2i m_BoardTopLeft;
+private: // Smooth stuff
     Stellar::KeyFrame m_BoardTopLeftKF;
+    Stellar::KeyFrame m_PieceOpacityKF;
     Memento<Vec2i> m_BoardTopLeftMemento;
 };
 
