b311503942b36c09cf4082a0bf9aad8490c16806
Mez0ry <mez0ry@mail.ru>
2025-01-11 17:40:08 +0100

    Added optional parameter for fn 'execute'

--- Chess/IEntityCommand.hpp
+++ Chess/IEntityCommand.hpp
@@ -1,13 +1,13 @@
 #ifndef __ENTITY_COMMAND_HPP__
 #define __ENTITY_COMMAND_HPP__
 #include "IEntity.hpp"
-
+#include <optional>
 class IEntityCommand
 {
 public:
     virtual ~IEntityCommand() {}
 
-    virtual void Execute() = 0;
+    virtual bool Execute(std::optional<float> dt) = 0;
 };
 
 #endif //! __ENTITY_COMMAND_HPP__
