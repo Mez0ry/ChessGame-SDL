e287ba1a3c950a5b0e60a29bfb28db9ec79b8b50
Mez0ry <mez0ry@mail.ru>
2025-01-11 17:40:35 +0100

    Implemented command 'MoveTo'

---
+++ Chess/MoveTo.cpp
@@ -0,0 +1,37 @@
+#include "MoveTo.hpp"
+#include "Lerp.hpp"
+
+MoveTo::MoveTo(Board &board, Core::Ref<Piece> piece, const SmoothMove &smooth_move) : m_Board(board),m_Piece(piece), m_SmoothMove(smooth_move)
+{
+
+}
+
+bool MoveTo::Execute(std::optional<float> dt)
+{
+    if(!m_Board.IsMakeableMove(m_Piece,m_SmoothMove.board_move_to))
+        return false;
+    
+    auto& keyframe = m_SmoothMove.keyframe;
+
+    if(keyframe.IsActionEmpty()){
+        keyframe.Setup(m_SmoothMove.frame_duration,[&](float t){
+            int dx = 0,dy = 0;
+            auto& piece_texture = m_Piece->GetTexture();
+
+            dx = Stellar::Lerp(m_SmoothMove.move_from.x,m_SmoothMove.move_to.x,m_SmoothMove.easing_type(t));
+            dy = Stellar::Lerp(m_SmoothMove.move_from.y,m_SmoothMove.move_to.y,m_SmoothMove.easing_type(t));
+            piece_texture.SetPosition({dx,dy});
+        });
+    }
+
+    if(dt){
+        keyframe.Update(dt.value());
+    }
+    
+    if(keyframe.IsFinished()){
+        m_Board.MakeMove(m_Piece,m_SmoothMove.board_move_to);
+        m_Board.SetTextureEntityPosition(m_Piece);
+        return true;
+    }
+    return false;
+}
---
+++ Chess/MoveTo.hpp
@@ -0,0 +1,30 @@
+#ifndef __MOVE_TO_COMMAND_HPP__
+#define __MOVE_TO_COMMAND_HPP__
+#include "IEntityCommand.hpp"
+#include "Board.hpp"
+#include "Easing.hpp"
+#include "Piece.hpp"
+
+struct SmoothMove{
+    SmoothMove() : easing_type(Stellar::Easing::EaseInSine), frame_duration(1){}
+    ~SmoothMove() = default;
+
+    Vec2i move_from, move_to, board_move_to;
+    Stellar::easing_type_t easing_type;
+    float frame_duration;
+    Stellar::KeyFrame keyframe;
+};
+/**
+ * @brief makes move relative to board
+ */
+class MoveTo : public IEntityCommand
+{
+private:
+    Board &m_Board;
+    Core::Ref<Piece> m_Piece;
+    SmoothMove m_SmoothMove;
+public:
+    MoveTo(Board &board, Core::Ref<Piece> piece,const SmoothMove& smooth_move);
+    bool Execute(std::optional<float> dt) override;
+};
+#endif //!__MOVE_TO_COMMAND_HPP__
