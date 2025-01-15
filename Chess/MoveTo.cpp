10f27c3798ef8b0965c133f7adc266746ed407c1
Mez0ry <mez0ry@mail.ru>
2025-01-15 22:01:22 +0100

    Renamed member variable m_SmoothMove -> m_Move

--- Chess/MoveTo.cpp
+++ Chess/MoveTo.cpp
@@ -2,9 +2,9 @@
 #include "Lerp.hpp"
 #include "Move.hpp"
 
-MoveTo::MoveTo(Board &board, Core::Ref<Piece> piece, const Move &smooth_move) : m_Board(board),m_Piece(piece), m_SmoothMove(smooth_move)
+MoveTo::MoveTo(Board &board, Core::Ref<Piece> piece, const Move &smooth_move) : m_Board(board),m_Piece(piece), m_Move(smooth_move)
 {
-    //board.GeneratePossibleMoves(piece);
+    
 }
 
 CommandStatus MoveTo::Execute(std::optional<float> dt)
@@ -13,21 +13,20 @@ CommandStatus MoveTo::Execute(std::optional<float> dt)
         m_Board.GenerateLegalMoves(piece);
     });
 
-    if(!m_Board.IsMakeableMove(m_Piece,m_SmoothMove.board_move_to)){
+    if(!m_Board.IsMakeableMove(m_Piece,m_Move.board_move_to)){
         m_Board.SetTextureEntityPosition(m_Piece);
         return CommandStatus::FAILED;
     }
-    //if is there any pieces to kill
 
-    auto& keyframe = m_SmoothMove.keyframe;
+    auto& keyframe = m_Move.keyframe;
 
     if(keyframe.IsActionEmpty()){
-        keyframe.Setup(m_SmoothMove.frame_duration,[&](float t){
+        keyframe.Setup(m_Move.frame_duration,[&](float t){
             int dx = 0,dy = 0;
             auto& piece_texture = m_Piece->GetTexture();
 
-            dx = Stellar::Lerp(m_SmoothMove.move_from.x,m_SmoothMove.move_to.x,m_SmoothMove.easing_type(t));
-            dy = Stellar::Lerp(m_SmoothMove.move_from.y,m_SmoothMove.move_to.y,m_SmoothMove.easing_type(t));
+            dx = Stellar::Lerp(m_Move.move_from.x,m_Move.move_to.x,m_Move.easing_type(t));
+            dy = Stellar::Lerp(m_Move.move_from.y,m_Move.move_to.y,m_Move.easing_type(t));
             piece_texture.SetPosition({dx,dy});
         });
     }
@@ -36,14 +35,14 @@ CommandStatus MoveTo::Execute(std::optional<float> dt)
         keyframe.Update(dt.value());
     }
 
-    if(keyframe.GetElapsedFrames() > m_SmoothMove.frame_duration * 0.9f){
-        if(m_SmoothMove.piece_to_kill){
-            m_SmoothMove.piece_to_kill->Kill();
+    if(keyframe.GetElapsedFrames() > m_Move.frame_duration * 0.9f){
+        if(m_Move.piece_to_kill){
+            m_Move.piece_to_kill->Kill();
         }
     }
     
     if(keyframe.IsFinished()){
-        m_Board.MakeMove(m_Piece,m_SmoothMove.board_move_to);
+        m_Board.MakeMove(m_Piece,m_Move);
 
         m_Board.SetTextureEntityPosition(m_Piece);
         m_Board.GenerateLegalMoves(m_Piece);
--- Chess/MoveTo.hpp
+++ Chess/MoveTo.hpp
@@ -15,9 +15,9 @@ class MoveTo : public IEntityCommand
 private:
     Board &m_Board;
     Core::Ref<Piece> m_Piece;
-    Move m_SmoothMove;
+    Move m_Move;
 public:
-    MoveTo(Board &board, Core::Ref<Piece> piece,const Move& smooth_move);
+    MoveTo(Board &board, Core::Ref<Piece> piece,const Move& move);
     CommandStatus Execute(std::optional<float> dt) override;
 };
 #endif //!__MOVE_TO_COMMAND_HPP__
